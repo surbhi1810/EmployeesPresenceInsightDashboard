3ab32d1eb8ff0e55efd1eb39761610355b633a78
surbhigupta <surbhigupta396@gmail.com>
2024-08-25 13:40:22 +0530

    datatypesadded

---
+++ datatypes.py
@@ -0,0 +1,226 @@
+x = 1
+y = x
+# print(x)
+# print(y)
+# x = x+1
+# print(x)
+# print(y)
+# #id()
+# print(id(x))
+# print(id(y))
+# print("Hello world")
+# x = '21' 
+# y=int(x)x = 'a'
+
+
+# x[0] = S
+# x[1] = a
+#x[-1] = e
+#Slicing of a string - 
+# Syntax - stringname[start_index:end_index(excluded):steps]
+
+# print(x[::-1])
+# p = 5
+# y = "Hello"
+# # z = y + " "+ x + p
+# z = f"Hello{p}"
+
+
+
+# print("R")
+# print(x)
+
+# x = int(input("enter a number"))
+# print(type(x))
+
+# print(f"the number you have entered is {x}")
+
+# x = "Sasken is \'located in Bangalore"
+# x= x[0:7].upper() + x[7::]
+# print(x)
+
+# int x[5] = {10,20,30,40,50}
+# x = [10,"Sasken",8.9]
+# x = [10,60,30,40]
+# y = [50,60,70]
+# # append() - inserting a new element to the list 
+# # x.insert(1,y)
+# # x[1] = 'Sasken'
+# # x.clear()
+# # print(x)
+# # del x
+# # print(x)
+# # x.sort()
+# # print(x)
+
+# # for i in x :
+# #     print(i)
+# # x = x[0:3]
+# # print(x)
+
+# x = (10,60,30,40,30)
+# # print(type(x))
+# # print(x[0:3])
+# # x[0] = 20
+# x = list(x)
+# x[0] = 20
+# # print(x)
+# x.append(19)
+# x = tuple(x)
+# # print(type(x))
+# # print(x)
+# # for i in x:
+# #     print(i,end='\t')
+# # print(x.index(30))
+
+# x = {10,60,30,40,30}
+# y = {50,60,70}
+# print(x)
+# # print(x[0])
+# x.add(16)
+# print(x)
+# x.remove(60)
+# print(x)
+# x.pop()
+# print(x)
+# del x
+# print(x)
+# x.clear()
+# print(x)
+# for i in x:
+#     print(i)
+# x.update(y)
+# print(x)
+
+
+thisdict = {
+  "car1":{"brand": "Ford",
+  "model": "Mustang",
+  "year": 1964}
+ 
+ 
+
+}
+# print(thisdict["brand"])
+# for i in thisdict:
+#     print(thisdict[i]) # print(thisdict["brand"]) print(thisdict["model"]) print(thisdict["year"])
+
+
+# # thisdict["color"] = "red"
+# thisdict.update({"color":"red"})
+# # thisdict.pop("model")
+# # thisdict.clear()
+# print(thisdict)
+# #[(brand,Ford),(model,Mustang),(),()]
+# for i in thisdict.items():
+#     print(i)
+
+myfamily = {
+  "child1" : {
+    "name" : ["Emil",10,20],
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+myfamily2 = {
+  "child1" : {
+    "name" : ["Emil",10,20],
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+# arr = [myfamily,myfamily2]
+# print(myfamily["child1"]["name"][0])
+
+a = 10
+b = 9
+# if(a>b):
+#     print("Correct")
+# else:
+#     print("Wrong")
+
+x = {10,60,30,40,30}
+y = []
+if(bool(y)==True):
+    print("Correct")
+else:
+    print("Empty")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
